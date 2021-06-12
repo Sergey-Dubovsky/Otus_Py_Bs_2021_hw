19008275dace56d87a6936ba835547de858df2a0
Sergey-Dubovsky <85638802+Sergey-Dubovsky@users.noreply.github.com>
2021-06-12 22:11:20 +0300

    add power_numbers

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,29 +4,36 @@
 """
 
 
-def power_numbers():
+def power_numbers(list_of_numbers):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
+    list_of_square=[]
+
+    for num in list_of_numbers:
+        list_of_square.append(num**2)
+
+    return list_of_square    
 
 
-# filter types
-ODD = "odd"
-EVEN = "even"
-PRIME = "prime"
+
+# # filter types
+# ODD = "odd"
+# EVEN = "even"
+# PRIME = "prime"
 
 
-def filter_numbers():
-    """
-    функция, которая на вход принимает список из целых чисел,
-    и возвращает только чётные/нечётные/простые числа
-    (выбор производится передачей дополнительного аргумента)
+# def filter_numbers():
+#     """
+#     функция, которая на вход принимает список из целых чисел,
+#     и возвращает только чётные/нечётные/простые числа
+#     (выбор производится передачей дополнительного аргумента)
 
-    >>> filter_numbers([1, 2, 3], ODD)
-    <<< [1, 3]
-    >>> filter_numbers([2, 3, 4, 5], EVEN)
-    <<< [2, 4]
-    """
+#     >>> filter_numbers([1, 2, 3], ODD)
+#     <<< [1, 3]
+#     >>> filter_numbers([2, 3, 4, 5], EVEN)
+#     <<< [2, 4]
+#     """
